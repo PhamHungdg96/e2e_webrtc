615efb1fa63ee1947441b98c55e8dd20245a6807
phamhungdg96 <phamhungdg96@gmail.com>
2020-11-23 16:40:25 +0700

    start

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname })
 })
+app.get('/demo', (req, res) => {
+  res.sendFile('public/demo.html', { root: __dirname })
+})
+
+app.get('/group_demo', (req, res) => {
+  res.sendFile('public/group_demo.html', { root: __dirname })
+})
+app.get('/one_to_one_demo', (req, res) => {
+  res.sendFile('public/one_to_one_demo.html', { root: __dirname })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
